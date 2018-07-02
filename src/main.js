3783195d364f4bfe363bd6f6cea8741d58992bb1
Tommyduong23 <tommyduong23@gmail.com>
2018-07-02 09:52:36 -0700

    initial release

--- src/main.js
+++ src/main.js
@@ -1,1 +1,80 @@
-// Put what you want to export in this file
+const generateKey = ( resizeListeners ) => {
+
+	let key = Date.now();
+
+	while ( resizeListeners[key] ) {
+		key += 1;
+	}
+
+	return key;
+
+};
+
+
+export default ( function ( delay ) {
+
+	const resizeListeners = {};
+
+	let timeout = null;
+
+	const getDims = () => {
+		const { innerHeight : height, innerWidth : width } = window;
+
+		return { height, width };
+	};
+
+	// watch for resize {delay} seconds
+	// after the page resize is complete
+	window.addEventListener( 'resize', ( e ) => {
+
+		if ( timeout ) {
+			// resize events frequently happen hundreds of times
+			// in seconds. so this makes sure we only run resize
+			// callbacks after we are sure the user is done resizing
+			// the page.
+			window.clearTimeout( timeout );
+		}
+
+		timeout = window.setTimeout( () => {
+
+			const keys = Object.keys( resizeListeners );
+			const dims = getDims();
+
+			keys.forEach( ( key ) => {
+				const callback = resizeListeners[key];
+
+				if ( typeof callback !== 'function' ) {
+					// some browsers leave null after
+					// you delete a key
+					return;
+				}
+
+				callback( dims );
+			} );
+
+		}, delay );
+
+	} );
+
+	return {
+
+		resize( callback, runImmediately = false ) {
+
+			const key = generateKey( resizeListeners );
+			resizeListeners[key] = callback;
+
+			if ( runImmediately ) {
+				callback( getDims() );
+			}
+
+			return key;
+
+		},
+
+		off( key ) {
+			delete resizeListeners[key];
+		}
+
+	};
+
+}( 200 ) ); // milliseconds to wait until running after resize ends
